8a29b49eff5fc08d949916a0c52b054f06b3fff8
SkullCutter02 <64243477+SkullCutter02@users.noreply.github.com>
2020-12-06 10:18:21 +0800

    Added homepage

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ app.use(express.json());
 app.use("/films", require("./routes/films"));
 app.use("/actors", require("./routes/actors"));
 
+app.get("/", (req, res) => {
+  res.send("<h1>Film API</h1>");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, async () => {
